f1afd566ba1ac45a3bce9e3738a015a799c8acb9
Zachiel <ezehiel182@gmail.com>
2023-04-16 15:23:52 +0200

    Starter project

---
+++ EisenhowerMain.cs
@@ -0,0 +1,7 @@
+﻿namespace EisenhowerCore
+{
+	public class EisenhowerMain
+	{
+		public static void Main(string[] args) => Console.WriteLine("Hello Codecool!");
+	}
+}
---
+++ TodoItem.cs
@@ -0,0 +1,5 @@
+namespace EisenhowerCore
+{
+	public class TodoItem
+	{ }
+}
