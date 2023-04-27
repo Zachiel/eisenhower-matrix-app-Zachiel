78016049d28267e974821c187cd2803a526ee67d
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2023-04-27 10:59:43 +0200

    Lines formatted

--- Display.cs
+++ Display.cs
@@ -1,73 +1,72 @@
 ﻿using System;
 namespace EisenhowerCore
 {
-	public class Display
-	{
-		public Display()
-		{
-		}
+    public class Display
+    {
+        public Display()
+        {
+        }
 
-		private string Header()
-		{
-			return " |                URGENT                  |              NOT URGENT              | ";
+        private string Header()
+        {
+            return " |                URGENT                 |              NOT URGENT              | ";
 
         }
 
         private string BreakLine()
         {
-            return "-|----------------------------------------|--------------------------------------|-";
+            return "-|---------------------------------------|--------------------------------------|-";
 
         }
 
-        private List<string> Lines(TodoQuarter urgent, TodoQuarter notUrgent)
+        private List<string> Lines(TodoQuarter urgent, TodoQuarter notUrgent, int lineWidth=37)
         {
-			List<string> lines = new List<string>();
+            List<string> lines = new List<string>();
 
-			List<TodoItem> urgentItems = urgent.GetAssignedItems();
+            List<TodoItem> urgentItems = urgent.GetAssignedItems();
             List<TodoItem> notUrgentItems = notUrgent.GetAssignedItems();
 
-			int urgentCounter = urgentItems.Count();
+            int urgentCounter = urgentItems.Count();
             int notUrgentCounter = notUrgentItems.Count();
-			int max;
-			if (urgentCounter < 13 && notUrgentCounter < 13) max = 13;
-			else if (urgentCounter > notUrgentCounter) max = urgentCounter - 1;
-			else max = notUrgentCounter - 1;
-
-            for (int i = 0; i<=max; i++)
-			{
-				if (i < urgentCounter && i < notUrgentCounter)
-				{
-					lines.Add($" | {urgentItems[i].ToString()}  | {notUrgentItems[i].ToString()} | ");
-				}
-				else if (i < notUrgentCounter)
-				{
-                    lines.Add($" |                                    | {notUrgentItems[i].ToString()} | ");
+            int max;
+            if (urgentCounter < 13 && notUrgentCounter < 13) max = 13;
+            else if (urgentCounter > notUrgentCounter) max = urgentCounter - 1;
+            else max = notUrgentCounter - 1;
+            for (int i = 0; i <= max; i++)
+            {
+                if (i < urgentCounter && i < notUrgentCounter)
+                {
+                    lines.Add($" | {urgentItems[i].ToString().PadRight(lineWidth)} | {notUrgentItems[i].ToString().PadRight(lineWidth)} | ");
+                }
+                else if (i < notUrgentCounter)
+                {
+                    lines.Add($" |                                   | {notUrgentItems[i].ToString().PadRight(lineWidth)} | ");
                 }
                 else if (i < urgentCounter)
                 {
-                    lines.Add($" | {urgentItems[i].ToString()}  |                                     | ");
+                    lines.Add($" | {urgentItems[i].ToString().PadRight(lineWidth)} |                                      | ");
                 }
-				else
-				{
-                    lines.Add($" |                                    |                                     | ");
+                else
+                {
+                    lines.Add($" |                                       |                                      | ");
                 }
 
-            }	
+            }
 
             return lines;
 
         }
 
         public void DisplayMatrix(TodoMatrix Matrix)
-		{
-			Console.WriteLine(Header());
-			Console.WriteLine(BreakLine());
-			List<string> importantLines = Lines(Matrix._quarters[0], Matrix._quarters[1]);
-			foreach (string line in importantLines)
-			{
-				Console.WriteLine(line);
-			}
-			Console.WriteLine(BreakLine());
+        {
+            Console.WriteLine(Header());
+            Console.WriteLine(BreakLine());
+            List<string> importantLines = Lines(Matrix._quarters[0], Matrix._quarters[1]);
+            foreach (string line in importantLines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine(BreakLine());
             List<string> notImportantLines = Lines(Matrix._quarters[2], Matrix._quarters[3]);
             foreach (string line in notImportantLines)
             {
@@ -77,4 +76,3 @@ namespace EisenhowerCore
         }
     }
 }
-
